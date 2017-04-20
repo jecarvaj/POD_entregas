4f027ac155a6205c4d77cb7b7b8e1c1d8d3d2391
jonaladd <yonaladd@gmail.com>
2017-04-20 15:14:55 -0300

    se subio?

--- app/src/main/java/com/example/jean/prueba1/app/AppConfig.java
+++ app/src/main/java/com/example/jean/prueba1/app/AppConfig.java
@@ -6,7 +6,7 @@ package com.example.jean.prueba1.app;
 public class AppConfig {
 
     //Direccion IP  del servidor
-    public static String SERVER="http://192.168.1.109/";
+    public static String SERVER="http://192.168.0.10/";
 
     // URL de inicio de sesión del servidor
     public static String URL_LOGIN = SERVER+"prueba1/login.php";
